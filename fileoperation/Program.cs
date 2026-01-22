cbb651f0fb412bae9c655d856655b16d8206c331
Opilane TTHK <opilane@tthk.ee>
2026-01-22 11:41:58 +0200

    mdea

---
+++ fileoperation/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace fileoperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("´Kirjuta siia oma postkasti aadress");
+
+            //string filePath = "G:\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\postkastiadress.txt";
+            //string aadress = Console.ReadLine();
+
+            //File.WriteAllText(filePath, aadress);
+
+            Console.WriteLine("Kõik laulusõnad");
+
+            //DisplayThisFile();
+
+            FindWord("lima");
+        }
+        public static void FindWord(string findThisWord)
+        {
+            using (StreamReader readFile = new StreamReader(
+                "C:\\Users\\opilane\\source\\repos\\Scarlet\\fileoperation\\mjbj.txt"
+                ))
+            {
+                {
+                    int lnr = 0;
+                    while (readFile.EndOfStream == false)
+                    {
+                        string line = readFile.ReadLine();
+                        lnr++;
+
+                        if (line.Contains(findThisWord) == true)
+                        {
+                            Console.WriteLine(findThisWord + " leiti reast " + lnr);
+
+                        }
+                    }
+                    readFile.Close();
+                }
+            }
+        }
+        private static void DisplayThisFile()
+        {
+            string line = "";
+            try
+            {
+                using (
+                    StreamReader readThisThing = new StreamReader
+                    ("G\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\FileOperation\\mjbj.txt"))
+                {
+                    int lineCount = 0;
+                    while (readThisThing.EndOfStream == false)
+                    {
+                        lineCount++;
+                        line = readThisThing.ReadLine();
+                        int pikkus = line.Length;
+                        Console.WriteLine(lineCount + " " + line + " . Reapikkus on" + pikkus);
+                    }
+                    readThisThing.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ei saa lugeda sest:" + e.Message);
+                throw;
+            }
+        }
+    }
+}
